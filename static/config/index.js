6faa977d3802d8451b61138b40aca9d937889068
张滕民 <zhangtengmin@hasng.cn>
2020-12-15 21:37:17 +0800

    ceshi
    jdjjj

--- static/config/index.js
+++ static/config/index.js
@@ -6,9 +6,8 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://172.16.100.29:8080/';
-  // window.SITE_CONFIG['baseUrl'] = 'http://192.168.1.246:8080';
-
+  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080';
+  window.SITE_CONFIG['baseUrl'] = 'http://172.16.100.28:8080';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './'; // 域名
